6c43676511d098124916d56b381d2ff9fc9ef5fe
Logan Jewell <118623618+LoganJewel@users.noreply.github.com>
2023-03-19 16:49:48 -0400

    Finished Image Plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {StaticImage} from "gatsby-plugin-image";
 
 const Home = () =>{
   return (
     <main>
       <Layout pageHeading='Home' pageTitle='Home Page'>
         Welcome, this is my site!
+        <StaticImage src='../images/2 Regular Hedgehogs.jpg' alt='Just Some Hedgehogs'/>
     </Layout>
     </main>
   );
